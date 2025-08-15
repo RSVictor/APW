3973f6810a4f6abafac5089c1dcc8947e8accd8d
RSVictor <152220391+RSVictor@users.noreply.github.com>
2025-08-14 22:36:53 -0300

    actualization

--- Aula1/app/admin.py
+++ Aula1/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import*
+
+admin.site.register(Movie)
+admin.site.register(Director)
+admin.site.register(Plan)
+admin.site.register(UserPlan)
+admin.site.register(FavoriteMovie)
