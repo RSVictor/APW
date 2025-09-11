1bddbf71e4bc7fe380e8830b9eab2cd544d03c8d
RSVictor <152220391+RSVictor@users.noreply.github.com>
2025-09-11 19:06:02 -0300

    atualizado

--- 1-MANFLIX/Aula4-FRONTEND/manflix-front/src/services/movie.services.ts
+++
@@ -1,6 +0,0 @@
-import type { Movie } from "@/models/movies";
-import { getAxios } from "./services.config";
-
-export const getMovies = (): Promise<Array<Movie>>=>{
-    return getAxios().get('/api/movies/');
-}
--- Aula1/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import*
-
-admin.site.register(Movie)
-admin.site.register(Director)
-admin.site.register(Plan)
-admin.site.register(UserPlan)
-admin.site.register(FavoriteMovie)
